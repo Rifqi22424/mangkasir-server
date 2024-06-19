66280d93a17faa936a148361a4c16ed72c03ef5e
rifqi2242 <rifqimuzakki45@gmail.com>
2024-06-19 15:43:27 +0700

    add size to RegisterRequest

--- src/main/java/com/mangcoding/mangkasir/dto/RegisterRequest.java
+++ src/main/java/com/mangcoding/mangkasir/dto/RegisterRequest.java
@@ -3,6 +3,7 @@ package com.mangcoding.mangkasir.dto;
 // import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 // import jakarta.validation.constraints.Pattern;
 // import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 public class RegisterRequest {
 
     @NotBlank(message = "Username is mandatory")
+    @Size(min = 5, message = "Username must be at least 5 characters")
     private String username;
     
     @NotBlank(message = "Username is mandatory")
@@ -23,10 +25,9 @@ public class RegisterRequest {
     private String gmail;
 
     @NotBlank(message = "Password cannot be empty")
-    // @Size(min = 1, message = "Password cannot be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
     
     @NotBlank(message = "Confirm Password cannot be empty")
-    // @Size(min = 1, message = "Confirm Password cannot be empty")
     private String confirmPassword;
 }
